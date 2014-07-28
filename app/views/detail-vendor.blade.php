dfc018898a5fbe37b40189018ce7811ef91a05b2
phuocbinh171 <mtpbinh@gmail.com>
2014-07-28 11:54:17 +0700

    Detail-vendor
    aaaaaaaa

--- app/views/detail-vendor.blade.php
+++ app/views/detail-vendor.blade.php
@@ -1,16 +1,19 @@
 @extends('main')
 @section('title')
-Detail Vendor
+{{$vendor->name}}
+@endsection
+@section('nav-bar')
+@include('nav')
 @endsection
 @section('content')
 		<div class="row" id="infor-vendor">
 			<div class="container">
 			<div class="col-xs-12 col-sm-6 col-md-9" id="body-left">
 				<div class="row" id="top-left">
-						<div class="col-xs-4 col-sm-4" id="left-infor">
-							<a href="#" id="left-infor title-infor">Địa điểm đám cưới Đà Nẵng</a>
-							<div id="left-infor avata-vendor">
-								{{'<img class="img-responsive" src="data:image/jpeg;base64,' . base64_encode($vendor->avatar) . '" />'}}
+						<div class="col-xs-6 col-sm-4" id="left-infor">
+							<a href="#" onclick="window.location('{{URL::route('list-vendor')}}')" id="left-infor title-infor">Địa điểm đám cưới {{Vendor::find($vendor->id)->location()->get()->first()->name}}</a>
+							<div id="left-infor avata-vendor" >
+								{{'<img class="img-responsive" style="width:250px;height:600 "src="data:image/jpeg;base64,' . base64_encode($vendor->avatar) . '" />'}}
 								<div class="fb-like" data-href="https://www.facebook.com/thuna.1987" data-width="150px" data-layout="standard" data-action="like" data-show-faces="false" data-share="false"></div>
 								
 							</div>
@@ -24,7 +27,7 @@ Detail Vendor
 								}(document, 'script', 'facebook-jssdk'));
 							</script>
 						</div>
-						<div calss="col-xs-8 col-sm-6" id="right-infor">
+						<div calss="col-xs-6 col-sm-4" id="right-infor">
 							<h3 id="right-infor name">{{$vendor->name}}</h3>
 							<p id="right-infor address">{{$vendor->address}} {{Vendor::find($vendor->id)->location()->get()->first()->name}}<a href="#map" id="right-infor map"> |Map.</a></p>
 							<p id="right-infor web"><b>Website</b>:<a href="http://{{$vendor->website}}"id="right-infor link"> Ghé thăm Website của tôi</a></p>
@@ -33,15 +36,12 @@ Detail Vendor
 							</p>
 							<p id="right-infor catering"><b>Phục vụ</b>: Vui chơi, ẩm thức, giải trí, thư giãn...Sức chứa 200 người lớn.</p>
 							<p id="right-infor capaty"><b>Sức chứa</b>: 200 người lớn.</p>
-						
-
-
 						</div>
 
 					</div><!-- -End infor vendor -->
 					<div class="tab-content">
 					
-						<div class="tab-menu" >
+						<div class="tab-menu"  id="tabs">
 											
 								<!-- Nav tabs -->
 							<ul  class="nav nav-tabs" id="vendor-menu" role="tablist"  >
@@ -62,7 +62,52 @@ Detail Vendor
 								</div>
 								<div id="content-photo">
 									<h4>Ảnh</h4>
+										<ul id="thumbs-main">
+											<li class="active" rel="1"><img alt="" src="{{Asset("images/slide/1_thumb.jpg")}}"> </li>
+											<li rel="2"><img alt="" src="{{Asset("images/slide/2_thumb.jpg")}}"> </li>
+											<li rel="3"><img alt="" src="{{Asset("images/slide/3_thumb.jpg")}}"> </li>
+											<li rel="4"><img alt="" src="{{Asset("images/slide/4_thumb.jpg")}}"> </li>
+											<li rel="5"><img alt="" src="{{Asset("images/slide/5_thumb.jpg")}}"> </li>
+											<li rel="6"><img alt="" src="{{Asset("images/slide/6_thumb.jpg")}}"> </li>
+											<li rel="7"><img alt="" src="{{Asset("images/slide/7_thumb.jpg")}}"> </li>
+												   
+										</ul>
+										   
+									<a href="#photos"class="read-more">Xem thêm</a>
+
+
+															<!-- --Script click to image -->
+														<script type="text/javascript">
+															(function($) {
+																    $.fn.tabremote = function(options) {
+
+																          var settings = $.extend({
+																           panel: "#tabs",
+																           to: "data-to",
+																        }, options );
+
+																        $this=$(this);
+																        $panel=$(settings.panel);
+
+																    $this.click(function(){
+																                if($(this).attr("href"))
+																                    {var tos=$(this).attr("href");}
+																                else
+																                    {var tos=$(this).attr(settings.to);}
+
+																                to=tos.match(/\d/g);
+																    $panel.tabs({active: to-1});        
+																    return false;   
+																    });
+
+
+																        return this;
+																    }
+																})(jQuery);
+														</script>
+
 								</div>
+								<div style="clear:both"></div>
 
 								<div id="content-video">
 									<h4> Video</h4>
@@ -74,71 +119,74 @@ Detail Vendor
 						  			<h4>Review</h4>
 						  </div>
 						  <div class="tab-pane" id="photos">
-						  			<h4>Photos</h4>  <!-- Wrapper for slides -->
-					<div id="bigPic">
-						<img alt="" src="{{Asset("images/slide/1.jpg")}}">
-						<img alt="" src="{{Asset("images/slide/2.jpg")}}">
-						<img alt="" src="{{Asset("images/slide/3.jpg")}}">
-						<img alt="" src="{{Asset("images/slide/4.jpg")}}">
-						<img alt="" src="{{Asset("images/slide/5.jpg")}}">
-						<img alt="" src="{{Asset("images/slide/6.jpg")}}">
-						<img alt="" src="{{Asset("images/slide/7.jpg")}}">
-									    
-					</div>
+						  			  <!-- Wrapper for slides -->
+								<div id="bigPic">
+									<img alt="" src="{{Asset("images/slide/1.jpg")}}">
+									<img alt="" src="{{Asset("images/slide/2.jpg")}}">
+									<img alt="" src="{{Asset("images/slide/3.jpg")}}">
+									<img alt="" src="{{Asset("images/slide/4.jpg")}}">
+									<img alt="" src="{{Asset("images/slide/5.jpg")}}">
+									<img alt="" src="{{Asset("images/slide/6.jpg")}}">
+									<img alt="" src="{{Asset("images/slide/7.jpg")}}">
+												    
+								</div>
 
-					<ul id="thumbs">
-						<li class="active" rel="1"><img alt="" src="{{Asset("images/slide/1_thumb.jpg")}}"> </li>
-						<li rel="2"><img alt="" src="{{Asset("images/slide/2_thumb.jpg")}}"> </li>
-						<li rel="3"><img alt="" src="{{Asset("images/slide/3_thumb.jpg")}}"> </li>
-						<li rel="4"><img alt="" src="{{Asset("images/slide/4_thumb.jpg")}}"> </li>
-						<li rel="5"><img alt="" src="{{Asset("images/slide/5_thumb.jpg")}}"> </li>
-						<li rel="6"><img alt="" src="{{Asset("images/slide/6_thumb.jpg")}}"> </li>
-						<li rel="7"><img alt="" src="{{Asset("images/slide/7_thumb.jpg")}}"> </li>
-									   
-					</ul>
-									
-					<script type="text/javascript">
-						var currentImage;
-						var currentIndex = -1;
-						var interval;
-							function showImage(index){
-								if(index < $('#bigPic img').length){
-									var indexImage = $('#bigPic img')[index]
-									if(currentImage){   
-										if(currentImage != indexImage ){
-											$(currentImage).css('z-index',2);
-												clearTimeout(myTimer);
-											$(currentImage).fadeOut(250, function() {
-												myTimer = setTimeout("showNext()", 3000);
-											$(this).css({'display':'none','z-index':1})
-										 });
-									}
-							  }
-								$(indexImage).css({'display':'block', 'opacity':1});
-								currentImage = indexImage;
-								currentIndex = index;
-								$('#thumbs li').removeClass('active');
-								$($('#thumbs li')[index]).addClass('active');
-									}
-								}
+								<ul id="thumbs">
+									<li class="active" rel="1"><img alt="" src="{{Asset("images/slide/1_thumb.jpg")}}"> </li>
+									<li rel="2"><img alt="" src="{{Asset("images/slide/2_thumb.jpg")}}"> </li>
+									<li rel="3"><img alt="" src="{{Asset("images/slide/3_thumb.jpg")}}"> </li>
+									<li rel="4"><img alt="" src="{{Asset("images/slide/4_thumb.jpg")}}"> </li>
+									<li rel="5"><img alt="" src="{{Asset("images/slide/5_thumb.jpg")}}"> </li>
+									<li rel="6"><img alt="" src="{{Asset("images/slide/6_thumb.jpg")}}"> </li>
+									<li rel="7"><img alt="" src="{{Asset("images/slide/7_thumb.jpg")}}"> </li>
+												   
+								</ul>
+												
+								<script type="text/javascript">
+									var currentImage;
+									var currentIndex = -1;
+									var interval;
+										function showImage(index){
+											if(index < $('#bigPic img').length){
+												var indexImage = $('#bigPic img')[index]
+												if(currentImage){   
+													if(currentImage != indexImage ){
+														$(currentImage).css('z-index',2);
+															clearTimeout(myTimer);
+														$(currentImage).fadeOut(250, function() {
+															myTimer = setTimeout("showNext()", 3000);
+														$(this).css({'display':'none','z-index':1})
+													 });
+												}
+										  }
+											$(indexImage).css({'display':'block', 'opacity':1});
+											currentImage = indexImage;
+											currentIndex = index;
+											$('#thumbs li').removeClass('active');
+											$($('#thumbs li')[index]).addClass('active');
+												}
+											}
 
-								function showNext(){
-									var len = $('#bigPic img').length;
-									var next = currentIndex < (len-1) ? currentIndex + 1 : 0;
-								 	showImage(next);
-								}
+											function showNext(){
+												var len = $('#bigPic img').length;
+												var next = currentIndex < (len-1) ? currentIndex + 1 : 0;
+											 	showImage(next);
+											}
 
-										var myTimer;
+													var myTimer;
 
-											$(document).ready(function() {
-											myTimer = setTimeout("showNext()", 3000);
-											showNext(); //Load first image
-											$('#thumbs li').bind('click',function(e){
-											var count = $(this).attr('rel');
-											showImage(parseInt(count)-1);
+														$(document).ready(function() {
+														myTimer = setTimeout("showNext()", 3000);
+														showNext(); //Load first image
+														$('#thumbs li').bind('click',function(e){
+														var count = $(this).attr('rel');
+														showImage(parseInt(count)-1);
+														});
 											});
-								});
-											</script>	
+														</script>	
+
+
+
 						  </div>
 						  <div class="tab-pane" id="video">
 						  		
@@ -168,28 +216,28 @@ Detail Vendor
 			</div>
 			<div class="col-xs-6 col-md-3" id="right-contact">
 					<div class="contact-me">
-						<h3> <i class="glyphicon glyphicon-earphone"></i> 01234 856 856 </h3>
+						<h4> <i class="glyphicon glyphicon-earphone"></i> 01234 856 856 </h4>
 						<p>Xin vui lòng cho biết nhà cung cấp này bạn tìm thấy trên Thuna.vn</p>
 					<div  class="form">
 							<form class="form-horizontal" role="form">
 							  <div class="form-group">
 							    <div class="">
-							      <input type="text" class="form-control" id="input-firstname" value="Phước">
+							      <input type="text" class="form-control" id="input-firstname" placeholder="Họ">
 							    </div>
 							  </div>
 							  <div class="form-group">
 							    <div class="">
-							      <input type="text" class="form-control" id="Input-lastname" value="Bình">
+							      <input type="text" class="form-control" id="Input-lastname" placeholder="Tên">
 							    </div>
 							  </div>
 							  <div class="form-group">
 							    <div class="">
-							      <input type="email" class="form-control" id="input-email" value="aaa@gmail.com">
+							      <input type="email" class="form-control" id="input-email" placeholder="thuna@gmail.vn">
 							    </div>
 							  </div>
 							  <div class="form-group">
 							    <div class="">
-							      <input type="date" class="form-control" id="date-wedding" value="25/07/2014">
+							      <input type="date" class="form-control" id="date-wedding" placeholder="25/07/2014">
 							    </div>
 							  </div>
 							  <div class="form-group">
@@ -200,53 +248,55 @@ Detail Vendor
 									  <option value="3"> Đặt một câu hỏi</option>
 								  </select>
 							    </div><br/>
-							    <ul id="options" style="margin-left:-40px">
+							    <ul id="options" style="margin-left:10px">
 							  		<li style="display:none;list-style:none;list-style-position:inside; "><div id="number1" calss="number" ></div></li>
 								    <li style="display:none;list-style:none;list-style-position:inside;width:300px;margin-left: -20px;"><div id="number2" calss="number" >
 								    	 <div class="form-group">
 											<div class="">
-												   <input type="text" class="form-control" id="Input-phonenumber" value="" style="width:80%; margin-left: 20px;">
+												   <input type="text" class="form-control" id="Input-phonenumber" value="" style="width:80%; margin-left: -31px;">
 											</div>
 										</div>
-										<p style="margin-left:80px; ">e.g.xxx.xxx.xxxx</p>
-										<h5 style="margin-left:30px;; font-weight:bold " >Thời gian tốt nhất để liên lạc:</h5>
-										<div class="row">
-											<div class="col-xs-6 .col-sm-2">
-												<div class="checkbox">
-												  <label style="font-weight:bold ">
-												    <input type="checkbox" value="">
-													Bất kì thời gian
-												  </label>
-												</div>
+										<p style="margin-left:35px; ">e.g.xxx.xxx.xxxx</p>
+										<h6 style="margin-left:-45px; font-weight:bold " >Thời gian tốt nhất để liên lạc:</h6>
+										<div id="check-call">
+											<div class="row">
+												<div class="col-xs-6 col-md-5">
+													<div class="checkbox" style="margin-left:0px;">
+													
+													    <input type="checkbox" value="" >
+														Bất kì thời gian
+												
+													</div>
 
-												<div class="checkbox">
-												  <label style="font-weight:bold ">
-												    <input type="checkbox" value="">
-													Hằng ngày
-												  </label>
+													<div class="checkbox" style="margin-left:0px;">
+													
+													    <input type="checkbox" value="">
+														Hằng ngày
+													  
+													</div>
 												</div>
-											</div>
-											<div class="col-xs-6 .col-sm-4" >
-												<div class="checkbox">
-												  <label style="font-weight:bold ">
-												    <input type="checkbox" value="">
-													Mỗi đêm
-												  </label>
-												</div>
-												<div class="checkbox">
-												  <label style="font-weight:bold ">
-												    <input type="checkbox" value="">
-													Mỗi cuối tuần
-												  </label>
-												</div>
+												<div class="col-xs-6 col-md-5" >
+													<div class="checkbox" style="margin-left:5px;">
+													  
+													    <input type="checkbox" value="">
+														Mỗi đêm
+													
+													</div>
+													<div class="checkbox" style="margin-left:5px;">
+													 
+													    <input type="checkbox" value="">
+														Mỗi cuối tuần
+													
+													</div>
 
+												</div>
 											</div>
 										</div>
 
 								    </div></li><!-- -endcall-div -->
 								    
 								    <li style="display:none ; list-style:none"><div id="number3" class="number">
-								    	<textarea rows="7" cols="35" style="margin-left:-15px" >Vui lòng gởi thông tin về dịch vụ của chúng tôi.
+								    	<textarea rows="7" cols="21" style="margin-left:-68px; resize: none" >
 								    	</textarea>
 
 								    </div></li>
@@ -256,19 +306,15 @@ Detail Vendor
 							
 								  <script >
 
-									$("#selection-contact").change(function() {
+									$("#selection-contact").change(function(){
 										      var index = $(this).children(":selected").index();
 										      $("#options").children().hide().eq(index).show();
 										});
 
 				        			</script>
-
-								  </div>
-								  
-
 							  <div class="form-group" style="margin-left: 62px">
 							    <div class="">
-							      <button type="button" class="btn btn-primary btn-lg active" id="btn-contact"style="background-color: #f7dde7;border-color: #f7dde7"> Liên lạc</button>
+							      <button type="button" class="btn btn-skin btn-lg" id="btn-contact"> Liên lạc</button>
 							  </div>
 							</div>
 							 <div class="form-group"style="margin-left:72px">
