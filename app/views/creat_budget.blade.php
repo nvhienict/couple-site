44236fa86266d621548c865daf3a7baef57973b8
nguyenvantubk <nguyenvnantubk@gmail.com>
2014-08-19 09:26:20 +0700

    create_budget

--- app/views/creat_budget.blade.php
+++ app/views/creat_budget.blade.php
@@ -18,7 +18,7 @@ Budget
 					<div class="row form-group">
 						<label for="money_budget" class="col-xs-2 control-label">Số tiền</label>
 						<div class="col-xs-5">
-							<input type="text"  class="form-control" id="money_budget1" name="money_budget1" placeholder="Nhập số tiền"  />
+							<input type="text" onkeyup="key_money(event)" class="form-control" id="money_budget1" name="money_budget1" placeholder="Nhập số tiền"  />
 						   	<input type="text" hidden name="money_budget" id="money_budget"/>
 						</div>
 						<small>VND</small>
@@ -35,50 +35,64 @@ Budget
 				</form>
 				<script type="text/javascript">
 
-				$("#money_budget1").keypress(validateNumber);
-					function validateNumber(event) {
-					    var key = window.event ? event.keyCode : event.which;
-					    if (event.keyCode == 8 || event.keyCode == 46
-					     || event.keyCode == 37 || event.keyCode == 39) {
-					    	$(this).number(true);
-					        return true;
-					    }
-					    else if ( key < 48 || key > 57 ) {
+				// $("#money_budget1").keypress(validateNumber);
+				// 	function validateNumber(event) {
+				// 	    var key = window.event ? event.keyCode : event.which;
+				// 	    if (event.keyCode == 8 || event.keyCode == 46
+				// 	     || event.keyCode == 37 || event.keyCode == 39) {
+				// 	    	$(this).number(true);
+				// 	        return true;
+				// 	    }
+				// 	    else if ( key < 48 || key > 57 ) {
 
-					        return false;
-					    }
-					    else{
-					     $(this).number(true);
-					     return true;
+				// 	        return false;
+				// 	    }
+				// 	    else{
+				// 	     $(this).number(true);
+				// 	     return true;
 
-					    } true;
-					} 
+				// 	    } true;
+				// 	} 
 
-				$("#form-create-money").validate({
-					rules:
-					{
-						money_budget1:
-						{
-							required:true,
-							range:[50000000,1000000000]
-						}
-					},
-					messages:
-					{
-						money_budget1:
-						{
-							required:"Chưa nhập số tiền",
-							range:" Hệ thống chỉ tính toán vs số tiền từ 50 triệu tới 1 tỷ"
-						}
-					}
-				})
+				// $("#form-create-money").validate({
+				// 	rules:
+				// 	{
+				// 		money_budget1:
+				// 		{
+				// 			required:true,
+				// 			range:[50000000,1000000000]
+				// 		}
+				// 	},
+				// 	messages:
+				// 	{
+				// 		money_budget1:
+				// 		{
+				// 			required:"Chưa nhập số tiền",
+				// 			range:" Hệ thống chỉ tính toán vs số tiền từ 50 triệu tới 1 tỷ"
+				// 		}
+				// 	}
+				// })
 
-				$(function(){
-				    $("#money_budget1").keyup(function () {
-				        var value = $(this).val();
-				        $("#money_budget").val(value);
-				    }).keyup();
-				});
+				// $(function(){
+				//     $("#money_budget1").keyup(function () {
+				//         var value = $(this).val();
+				//         $("#money_budget").val(value);
+				//     }).keyup();
+				// });
+                  function key_money(event){
+                     if(event.which >= 37 && event.which <= 40) return;
+		                 $("#money_budget1").val(function(index, value) {
+						        return value
+						            .replace(/\D/g, '')
+						            .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+						            $('#money_budget').val($("#money_budget1").val().replace(/,/gi,''));
+						    });
+		                 $('#money_budget').val($("#money_budget1").val().replace(/,/gi,''));
+		                 
+		                 //alert(($("#money_budget1").val().replace(/,/gi,'')));
+		                 //alert($("money_budget").val());
+                   
+				};
 				
 				</script>
 			</div>
