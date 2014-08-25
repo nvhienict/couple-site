48db7378825721c103b4ae35572fb8b65a01d452
coquynho <thuy11i3.cit@gmail.com>
2014-08-25 10:31:30 +0700

    Database Guest
    tạo hai bảng dữ liệu, chạy bằng lệnh cmd: php artisan migrate
    --path=app/database/migrations/guest
    ok

---
+++ app/database/migrations/guest/2014_08_25_030504_create_Guest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuest extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('guests',function($table){
+			$table->increments("id");
+			$table->string("fullname");
+			$table->string("email")->unique();
+			$table->string("addresss");
+			$table->string("phone");
+			$table->integer("attending");
+			$table->integer("group");
+			$table->boolean("invited");
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('guests');
+	}
+
+}
