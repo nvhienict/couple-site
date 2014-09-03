6ef3504c3a3228dff6ae681ab6b4d9cdb21da3c8
phuocbinh171 <mtpbinh@gmail.com>
2014-09-03 10:52:04 +0700

    Create table photo slide

---
+++ app/database/migrations/2014_09_03_034812_create_photoslide.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotoslide extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('photoslide',function($table)
+			{
+				$table->increments("id");
+				$table->integer('vendor');
+				$table->binary('bigpic');
+				$table->binary('smallpic');
+				$table->timestamps();
+			});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop("photoslide");
+	}
+
+}
