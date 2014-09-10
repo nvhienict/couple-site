46757a6ec3d32d583aca78a8fc7e7380056276a5
phuocbinh171 <mtpbinh@gmail.com>
2014-09-10 12:27:33 +0700

    upload image

--- app/views/website_user/page_design.blade.php
+++ app/views/website_user/page_design.blade.php
@@ -19,7 +19,7 @@
 	<!-- .heading -->
 
 	<div class="row">
-		<div class="col-xs-1 design_website_content_left_hide">
+<div class="col-xs-1 design_website_content_left_hide">
 			<a href="javascript:;" onclick="design_website_plus_circle();" ><i class='fa fa-plus-circle fa-fw'></i></a>
 		</div>
 
@@ -27,15 +27,12 @@
 			<div class="minus-plus">
 				<a href="javascript:;" onclick="design_website_minus_circle();"><i class='fa fa-minus-circle fa-fw'></i></a>
 			</div>
-			
 			<!-- Nav tabs -->
 			<ul class="nav nav-tabs" role="tablist">
 			  	<li class="active"><a href="#page_design_home" role="tab" data-toggle="tab">Thiết kế</a></li>
 			  	<li><a href="#design_page" role="tab" data-toggle="tab">Trang Web</a></li>
 			  	<li><a href="#design_setup" role="tab" data-toggle="tab">Cài đặt</a></li>
-			  	
 			</ul>
-			
 
 			<!-- Tab panes -->
 			<div class="tab-content" style="padding: 5px;">
@@ -47,9 +44,83 @@
 			  		<div class="page_design_home_item">
 			  			<span class="span_design_item">Hình nền:</span><br />
 			  			<img src="{{Asset('images/website/1.png')}}">
-			  			<button class="btn btn-primary" style="background: #19b5bc; border:none;">Đổi ảnh</button>
-			  		
+			  			<button class="btn btn-primary" data-toggle="modal" data-target='#modal-changebackground' style="background: #19b5bc; border:none;">Đổi Ảnh Nền</button>
+			  			<!-- -modal change background -->
+			  				<div class="modal fade " id="modal-changebackground">
+								<div class="modal-dialog modal-lg">
+									<div class="modal-content ">
+										<div class="modal-header">
+											<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+											<h4 class="modal-title">Chọn Ảnh Nền</h4>
+										</div>
+										<div class="modal-body">
+											<div class="row">
+												<div class="col-xs-4 col-md-2 menu-image" >
+													<div class="menu-tab-modal">
+														<ul class="nav nav-pills nav-stacked" id="#my-menu-tab">
+															<li><a style="text-align:center"data-toggle="tab" href="#tab-modal-1">Upload Ảnh</a></li>
+															<li class="active"><a style="text-align:center" data-toggle="tab" href="#tab-modal-2">Ảnh Của Tôi</a></li>
+														</ul>
+													</div>
+												</div>
+												<div class="col-xs-12 col-md-10 tab-image">
+													<div class="tab-content ">
+														<div class="tab-pane " id="tab-modal-1">
+																<div class="upload-image-tab">
+																		
+																	<form action="" method="POST" role="form">
+																		
+																	
+																		<div class="form-group">
+																			<br>
+																			<br>
+																			<br>
+																			<br>
+																			<br>
+																			<br>
+																			<br>
+																			<br>
+
+																			<a onclick="chose_image()" id="chose_image" ><span class="glyphicon glyphicon-picture"></span>Chọn ảnh từ máy tính của bạn</a>
+																			<input id="input-image-modal"  type="file" class="file" multiple="true">
+																			
+																			
+																		</div>
+																																																			
+																		
+																		<script type="text/javascript">
+																		
+																			function chose_image()
+																			{
+																				 $('#input-image-modal').trigger('click');
+																			};
+																		</script>
+
+																	</form>
+																</div>
+
+														</div>
+														<div class="tab-pane active" id="tab-modal-2">
+														
+														</div>
+													</div>																																					
+												</div>
+
+											</div>
+											
+										</div>
+										
+											
+										
+									</div><!-- /.modal-content -->
+								</div><!-- /.modal-dialog -->
+							</div><!-- /.modal -->
+
+			  			<!-- -end modal change background -->
 			  			<hr>
+			  			
+
+			  		
 			  			<span class="span_design_item">Font chữ:</span><br />
 			  			<span class="span_design_item">Nội dung:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<select name="font_website" class="select_design">
 						  							<option value="">Arial</option>
@@ -75,49 +146,41 @@
 			  	<div class="tab-pane" id="design_page">
 			  		<table class="website_tabs">
 						<tr class="odd">
-							<td><input type="text" name="" value="1" size="2" class="website_tabs_input"></td>
 							<td>Welcome</td>
 							<td><span id="giang" class="glyphicon glyphicon-cog"></span></td>
 						</tr>
 						<tr class="even">
-							<td><input type="text" name="" value="2" size="2" class="website_tabs_input"></td>
 							<td>About</td>
 							<td><span class="glyphicon glyphicon-cog"></span></td>
 						</tr>
 						<tr class="odd">
-							<td><input type="text" name="" value="3" size="2" class="website_tabs_input"></td>
 							<td>Sự kiện cưới</td>
 							<td><span class="glyphicon glyphicon-cog"></span></td>
 						</tr>
 						<tr class="even">
-							<td><input type="text" name="" value="4" size="2" class="website_tabs_input"></td>
 							<td>Du lịch</td>
 							<td><span class="glyphicon glyphicon-cog"></span></td>
 						</tr>
 						<tr class="odd">
-							<td><input type="text" name="" value="5" size="2" class="website_tabs_input"></td>
 							<td>Đăng ký</td>
 							<td><span class="glyphicon glyphicon-cog"></span></td>
 						</tr>
 						<tr class="even">
-							<td><input type="text" name="" value="6" size="2" class="website_tabs_input"></td>
 							<td>Album ảnh</td>
 							<td><span class="glyphicon glyphicon-cog"></span></td>
 						</tr>
 						<tr class="odd">
-							<td><input type="text" name="" value="7" size="2" class="website_tabs_input"></td>
 							<td>Liên lạc</td>
 							<td><span class="glyphicon glyphicon-cog"></span></td>
 						</tr>
 						<tr class="even">
-							<td><input type="text" name="" value="8" size="2" class="website_tabs_input"></td>
 							<td>Lời chúc</td>
 							<td><span class="glyphicon glyphicon-cog"></span></td>
 						</tr>
 					</table>
 			  	</div>
 			  	<div class="tab-pane" id="design_setup">
-			  		
+			  	
 			  	</div>
 			</div>
 		</div>
@@ -128,8 +191,7 @@
 
 	</div>
 	<!-- .row -->
-
-<script type="text/javascript">
+	<script type="text/javascript">
 	function design_website_minus_circle(){
 		$('div.design_website_content_left').hide();
 		$('div.design_website_content_right').removeClass('col-xs-9 design_website_content_right').addClass('col-xs-11 design_website_content_right');
