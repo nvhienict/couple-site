68395dbbcd21a432cd6c2b2dec717889b6533414
phuocbinh171 <mtpbinh@gmail.com>
2014-09-10 13:05:00 +0700

    model WeddingWebsite

---
+++ app/models/WeddingWebsite.php
@@ -0,0 +1,20 @@
+<?php
+
+class WeddingWebsite extends Eloquent {
+
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'weddingwebsite';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	
+
+}
