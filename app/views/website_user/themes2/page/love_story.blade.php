d6cac8bdd290b10521f06ab3778f646599a7150c
giangdongmai <prdonggiang@gmail.com>
2014-10-01 15:50:01 +0700

    time count down

---
+++ app/views/website_user/themes2/page/love_story.blade.php
@@ -0,0 +1,19 @@
+
+
+<div class="partion">
+	<div class="row phara-margin">
+       	<h3 class="text-center title-tab" style="text-align: {{$tabWeb->titlestyle}}; font-familly: {{$website_item->font}}; color: #{{$website_item->color2}} " id = "nameTitle{{$tabWeb->id}}">
+            {{$tabWeb->title}}
+        </h3>
+
+        <div class="col-md-12 show-content phara{{$tabWeb->id}}">
+        	<p style="color: #{{$website_item->color3}}">
+                {{$tabWeb->content}}
+            </p>
+        </div>
+        
+
+    </div>
+
+
+</div>
