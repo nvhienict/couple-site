9ec66c921987ba1b226846e9f6f1f0f020b82f0a
phuocbinh171 <mtpbinh@gmail.com>
2014-10-02 14:59:51 +0700

    themes6

--- app/views/website_user/themes6/edit/contact.blade.php
+++ app/views/website_user/themes6/edit/contact.blade.php
@@ -10,7 +10,7 @@
                     <iframe width="700" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=vi&amp;geocode=&amp;q=Nh%C3%A0+H%C3%A0ng+Ph%C3%AC+L%C5%A9+3,+%C4%90%C3%A0+N%E1%BA%B5ng&amp;aq=&amp;sll=16.067021,108.220977&amp;sspn=0.098314,0.169086&amp;gl=VN&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;ll=16.047555,108.223128&amp;spn=0.006295,0.006295&amp;t=m&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=vi&amp;geocode=&amp;q=Nh%C3%A0+H%C3%A0ng+Ph%C3%AC+L%C5%A9+3,+%C4%90%C3%A0+N%E1%BA%B5ng&amp;aq=&amp;sll=16.067021,108.220977&amp;sspn=0.098314,0.169086&amp;gl=VN&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;ll=16.047555,108.223128&amp;spn=0.006295,0.006295&amp;t=m" style="color:#0000FF;text-align:left">Xem Bản đồ cỡ lớn hơn</a></small>
                </span><br>
                <span>
-                <button class="btn btn-primary button-hover" data-toggle="modal" data-target='#modal-changeimage' style="background: #19b5bc; border:none;">Đổi Địa chỉ</button>
+                <button class="btn btn-primary button-hover" style="background: #19b5bc; border:none;">Đổi Địa chỉ</button>
                 </span>  
            </div>                                   
               
