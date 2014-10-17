3e95af7b12d897161650fe8a4acf3931dd87ede8
giangmd <prdonggiang@gmail.com>
2014-10-17 09:09:08 +0700

    update migration

--- app/database/migrations/2014_08_18_075302_create_user.php
+++ app/database/migrations/2014_08_18_075302_create_user.php
@@ -24,7 +24,7 @@ class CreateUser extends Migration {
 				$table->date('weddingdate');
 				$table->integer('role_id');
 				$table->string('remember_me');
-				$table->float('budget');
+				$table->double('budget', 30, 0);
 				$table->timestamps();
 			});
 	}
