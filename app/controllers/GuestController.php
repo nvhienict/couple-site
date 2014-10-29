6b8b6004cc4a6d344c415dc359624b7e384b453d
giangmd <prdonggiang@gmail.com>
2014-10-29 11:10:32 +0700

    modyfi

--- app/controllers/GuestController.php
+++ app/controllers/GuestController.php
@@ -462,7 +462,7 @@ public function update_name()
 		}
 
 
-		Excel::create('Budget', function($excel) use($row) {
+		Excel::create('Guestslist', function($excel) use($row) {
 			
 		    $excel->sheet('Guestslist', function($sheet) use($row) {
 		    	
