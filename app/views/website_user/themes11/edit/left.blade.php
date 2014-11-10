de5726c2eb833039604af5f4e319d09968efea0a
nguyenvantubk <nguyenvnantubk@gmail.com>
2014-11-10 14:26:32 +0700

    update theme11

--- app/views/website_user/themes11/edit/left.blade.php
+++ app/views/website_user/themes11/edit/left.blade.php
@@ -1,20 +1,22 @@
-<div class="item">                   
+<div class="item" >                   
     <div id="slide{{$i+2}}" class="masonry margin-partion" style="min-height:450px;" >
-        <div class="post-box{{$i+2}} col-sx-12 col-lg-6 col-md-6 col-sm-6 bg-images "> 
-            <?php 
-                $images=PhotoTab::where('tab',$tabWeb->id)->get()->first();
-                 ?>
-            @if($images)
-                <img  class="img-responsive" src="{{Asset("{$images->photo}")}}" alt="">
-            @else 
-                <img  class="img-responsive" src="{{Asset("images/website/themes1/images.jpg")}}" alt="">
+        <div id="prev_output{{$tabWeb->id}}" class="post-box{{$i+2}} col-sx-12 col-lg-6 col-md-6 col-sm-6 bg-images "> 
+            <a href="#">
+                <?php 
+                    $images=PhotoTab::where('tab',$tabWeb->id)->get()->first();
+                     ?>
+                @if($images)
+                    <img  class="img-responsive" src="{{Asset("{$images->photo}")}}" alt="">
+                @else 
+                    <img  class="img-responsive" src="{{Asset("images/website/themes1/images.jpg")}}" alt="">
 
-            @endif
+                @endif
+            </a>
             <button  onclick="send_id({{$tabWeb->id}})"  class="btn btn-primary" data-toggle="modal" data-target='#modal-changeimage' style="background: #19b5bc; border:none;">Đổi Ảnh</button>
         </div>
         <div class="post-box{{$i+2}} col-sx-12 col-lg-6 col-md-6 col-sm-6"> 
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <h3 class="title-tab TT{{$tabWeb->id}}" style="text-align: {{$tabWeb->titlestyle}} font-familly: {{$website_item->font}}; color: #{{$website_item->color2}}">
+                <h3 class="title-tab TT{{$tabWeb->id}}" style="text-align: {{$tabWeb->titlestyle}} ;font-familly: {{$website_item->font}}; color: #{{$website_item->color2}}">
                      {{$tabWeb->title}}
                 </h3>
             </div>
@@ -36,18 +38,19 @@
         <div class="col-xs-11"></div>
         <div class="col-xs-1 click-edit click-edit-hide{{$tabWeb->id}}" >
             <span> <a  onclick="showckeditor({{$tabWeb->id}})" class="glyphicon glyphicon-edit icon-site" href="javascript:void(0);"></a></span>
-            <span><a class="glyphicon glyphicon-cog icon-site" href=""></a></span>
+           
         </div>
     </div>
     
     <div class="row phara-margin">
-        <div class="col-xs-11"></div>
-        <div class="col-xs-1 ok-edit ok-edit-show{{$tabWeb->id}}">
-            <span>
+        <div class="col-xs-10"></div>
+        <div class="col-xs-2 ok-edit ok-edit-show{{$tabWeb->id}}">
+            
+            <span style="float:right;"><a style="color:#e74c3c;" onclick="exitckeditor({{$tabWeb->id}})" class=" glyphicon glyphicon-remove icon-site" href="javascript:void(0);"></a></span>
+            <span style="float:right;" >
                 <a onclick="updateckeditor({{$tabWeb->id}})" class="glyphicon glyphicon-ok icon-site" href="javascript:void(0);"></a>
                 <input type="hidden" class="get_id{{$tabWeb->id}}" value="{{$tabWeb->id}}">
             </span>
-            <span><a style="color:#e74c3c;" onclick="exitckeditor({{$tabWeb->id}})" class=" glyphicon glyphicon-remove icon-site" href="javascript:void(0);"></a></span>
         </div>
     </div>
 </div>
