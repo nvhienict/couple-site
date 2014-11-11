bc53d01e64175fb3fb0568b256c57c382e0d8336
phuocbinh171 <mtpbinh@gmail.com>
2014-11-11 08:44:56 +0700

    fix themes 7

--- app/views/website_user/themes7/page/index.blade.php
+++ app/views/website_user/themes7/page/index.blade.php
@@ -10,7 +10,6 @@
 
    
     <link rel="stylesheet" type="text/css" href="{{Asset("assets/css/bootstrap.min.css")}}">
-     <link href="{{Asset("assets/font-awesome/css/font-awesome.min.css")}}" rel="stylesheet" type="text/css" />
     <script src="{{Asset('assets/ckeditor/ckeditor.js')}}"></script>
     <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
     <script src="{{Asset("assets/js/jquery.min.js")}}"></script>
@@ -43,63 +42,66 @@
            </div>
            <div  class="collapse navbar-collapse" id="example-navbar-collapse">
               <ul class="nav navbar-nav side-nav">                 
-                    @foreach(TabWebsite::where('website',$id_web)->where('visiable',0)->orderBy('sort','ASC')->get() as $menu_tab)
-                         <li><a class="{{$menu_tab->id}} scrollTo" href="#section_{{$menu_tab->type}}">{{$menu_tab->title}}</a></li>                
+                    <li class="active always-visible" ><a href="#home" role="tab" data-toggle="tab">Trang chủ</a></li>
+                    @foreach(TabWebsite::where('website',$id_web)->where('visiable',0)->orderBy('sort','ASC')->get() as $tab)
+                        <li><a href="#{{$tab->type}}" class="{{$tab->id}} TT{{$tab->id}}" role="tab" data-toggle="tab">{{$tab->title}}</a></li>                
                     @endforeach()
               </ul>
            </div>
-        </div>
     </nav>
 </div>
 <div class="container">        
-    <div class="row ">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 tab-content">
+                <div class="row tab-pane active" id="home">                     
+                    @include('website_user.themes7.page.main')                 
+                </div> 
         @foreach(TabWebsite::where('website',$id_web)->orderBy('sort','ASC')->get() as $tabWeb)
 
                 <!-- Welcome -->
                 @if($tabWeb->type =="welcome" && $tabWeb->visiable==0 )
-                    <div id="section_{{$tabWeb->type}}">
-                        @include('website_user.themes7.page.main')
+                    <div class="tab-pane" id="{{$tabWeb->type}}">
+                        @include('website_user.themes7.page.left')
                     </div>                
                 @endif
 
                 <!-- About Us -->
                 @if($tabWeb->type=="about" && $tabWeb->visiable==0)
-                    <div id="section_{{$tabWeb->type}}">
+                    <div class="tab-pane" id="{{$tabWeb->type}}">
                         @include('website_user.themes7.page.about_us')
                     </div>               
                 @endif
 
                 <!-- Wedding Event -->
                 @if($tabWeb->type=="wedding" && $tabWeb->visiable==0)
-                    <div id="section_{{$tabWeb->type}}">
+                    <div class="tab-pane" id="{{$tabWeb->type}}">
                         @include('website_user.themes7.page.left')
                     </div>              
                 @endif
 
                  <!-- Travaling -->
                 @if($tabWeb->type=="traval" && $tabWeb->visiable==0)
-                    <div id="section_{{$tabWeb->type}}">
+                    <div class="tab-pane" id="{{$tabWeb->type}}">
                      @include('website_user.themes7.page.right')
                     </div>           
                  @endif
 
                 <!-- Photo Album -->
                 @if($tabWeb->type=="album" && $tabWeb->visiable==0)
-                    <div id="section_{{$tabWeb->type}}">
+                    <div class="tab-pane" id="{{$tabWeb->type}}">
                         @include('website_user.themes7.page.photo')
                     </div>                              
                 @endif   
 
                 <!-- Contact Us -->
                 @if($tabWeb->type=="contact" && $tabWeb->visiable==0)
-                <div id="section_{{$tabWeb->type}}">
+                <div class="tab-pane" id="{{$tabWeb->type}}">
                     @include('website_user.themes7.page.contact')
                 </div>
                 @endif
 
                 <!-- cau chuyen tinh yeu -->
                 @if($tabWeb->type=="love_story")
-                    <div id="section_{{$tabWeb->type}}">
+                   <div class="tab-pane" id="{{$tabWeb->type}}">
                         @include('website_user.themes7.page.text')
                     </div>               
                 @endif            
@@ -107,37 +109,8 @@
 
             @endforeach          
     </div>
-    <div class="float-right icon-go_top" >
-            <a href="javascript:void(0);" class="btn btn-top" id="go_top">              
-                <i class="fa fa-angle-up fa-3x text-center"></i>
-            </a>
-    </div> 
+    
 </div>
 @endforeach
 @endif 
 
-<script type="text/javascript">
-        (function(){
-            // Cuộn trang lên với scrollTop
-            $('#go_top').click(function(){
-                $('body,html').animate({scrollTop:0},200);
-                return false;
-            })
-        })(jQuery)
-        $(window).scroll(function(){
-            if( $(window).scrollTop() > 200 ) {
-                $('#go_top').stop(false,true).fadeIn(200);
-            }else{
-                $('#go_top').hide();
-            }
-        });
-
-        //  $('.side-nav li').click(function(e) {
-        //     $('.side-nav li.active').removeClass('active');
-        //     var $this = $(this);
-        //     if (!$this.hasClass('active')) {
-        //         $this.addClass('active');
-        //     }
-        //     e.preventDefault();
-        // });
-</script>
