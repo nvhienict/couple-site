fc7f15af009f43b8468fbeb2ffaca17589043c4e
giangmd <prdonggiang@gmail.com>
2014-11-17 08:05:49 +0700

    modify

--- app/views/list-vendor-2.blade.php
+++ app/views/list-vendor-2.blade.php
@@ -343,7 +343,242 @@ Danh sách Dịch vụ
 			</div>
 			<!-- .tab-photo -->
 
-			
+			<!-- tab-list -->
+			<div class="col-md-7 tab-pane " id="display-list">
+
+			@if( count($results)==0 )
+				<h2>
+					Không có kết quả nào được tìm thấy
+				</h2>
+				@else
+					@if(!empty($compares))
+					<input type="hidden" id="count" name="a" value="{{count($compares)}}">
+
+							@foreach($results as $key=>$vendor)
+							<div class="row" id="show-list">
+								<div class="col-sm-4 col-lg-4 col-md-4">
+									<div class="list-avatar"><a href="{{URL::route('vendor',array(Category::where('id',Vendor::where('id',$vendor->id)->get()->first()->category)->get()->first()->slug,$vendor->slug))}}">{{'<img class="img-responsive" src="data:image/jpeg;base64,' . base64_encode($vendor->avatar) . '" />'}}</a></div>
+									<div class="list-category-name">{{Vendor::find($vendor->id)->location()->get()->first()->name}}</div>
+								</div>
+								<div class="col-sm-8 col-lg-8 col-md-8">
+									<div class="caption-list">
+			                            <div class="name"><a href="{{URL::route('vendor',array(Category::where('id',Vendor::where('id',$vendor->id)->get()->first()->category)->get()->first()->slug,$vendor->slug))}}">{{$vendor->name}}</a></div>
+			                         	<?php
+			                            	
+			                            	$about=strip_tags($vendor->about);
+			                            	if($about)
+			                            	{	
+			                            		$lengthstr=strlen($about);
+			                            		if($lengthstr<210)
+			                            		{
+			                            			$shortContent=$about;
+			                            		}
+			                            		else
+			                            		{
+			                            			$pos=strpos($about, ' ', 210);	                            		
+			                						$shortContent=substr($about,0,$pos )."..."; 	
+			                            		}	
+			                            		                            	
+			                            	}
+			                            	else
+			                            	{
+			                            		$shortContent="Chưa có mô tả về Dịch vụ này.";
+			                            	}	
+			                            	
+
+			                            ?>
+			                            <p>{{ $shortContent }}</p>
+			                            <div class="website">http://{{$vendor->website}}</div>
+			                        </div>
+									<div class="ratings">
+			                           <span class="pull-right">{{VendorComment::where('vendor',$vendor->id)->get()->count()}} Nhận xét</span>
+					   				             <p>
+					                                @if(Rating::where('vendor',$vendor->id)->get()->count()>0)
+					                                	@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1)==0)
+								  							<img src="{{Asset('images/star/0.jpg')}}" class="img-responsive agv-rating " alt="">
+								  						@else
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) > 0 & round(Rating::where('vendor',$vendor->id)->avg('rating'),1) < 1)
+									  							<img src="{{Asset('images/star/0.5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) == 1)
+									  							<img src="{{Asset('images/star/1.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) > 1 & round(Rating::where('vendor',$vendor->id)->avg('rating'),1) < 2)
+									  							<img src="{{Asset('images/star/1.5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) == 2)
+									  							<img src="{{Asset('images/star/2.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) > 2 & round(Rating::where('vendor',$vendor->id)->avg('rating'),1) < 3)
+									  							<img src="{{Asset('images/star/2.5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) == 3)
+									  							<img src="{{Asset('images/star/3.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) > 3 & round(Rating::where('vendor',$vendor->id)->avg('rating'),1) < 4)
+									  							<img src="{{Asset('images/star/3.5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) == 4)
+									  							<img src="{{Asset('images/star/4.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) > 4 & round(Rating::where('vendor',$vendor->id)->avg('rating'),1) < 5)
+									  							<img src="{{Asset('images/star/4.5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) == 5)
+									  							<img src="{{Asset('images/star/5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+								  						@endif
+					                                	@else
+					                                		<img src="{{Asset('images/star/0.jpg')}}" class="img-responsive agv-rating " alt="">
+					                                @endif
+					                            </p>
+			                        </div>
+
+			                        <!-- case vendor had in session compare -->
+				                    @if( in_array($vendor->id, $compares) )
+									<div class="compare-list">
+								        <label>
+									        <input checked type="checkbox" name="" value="" id="checkbox-list{{$vendor->id}}" class='compare-title'> Compare
+									        <input type="hidden" name="checkbox-{{$vendor->id}}" value="" >
+								        </label>
+								        <script type="text/javascript" src="{{Asset('assets/js/count-compare.js')}}"></script>
+								    </div>
+								    @else
+								    <div class="compare-list">
+								    	<label>
+									        <input type="checkbox" name="" value="" id="checkbox-list{{$vendor->id}}" class='compare-title'> Compare
+									        <input type="hidden" name="checkbox-{{$vendor->id}}" value="{{$vendor->id}}" >
+								        </label>
+								    </div>
+								    @endif
+
+								    <script type="text/javascript">
+									    $('#checkbox-list{{$vendor->id}}').on("click", function(){
+									    	$('#checkbox-photo{{$vendor->id}}').click();
+									    });
+								    </script>
+								</div>
+							</div>
+							@endforeach
+
+						@else
+						<input type="hidden" id="count" name="a" value="0">
+
+							@foreach($results as $key=>$vendor)
+							<div class="row" id="show-list">
+								<div class="col-sm-4 col-lg-4 col-md-4">
+									<div class="list-avatar"><a href="{{URL::route('vendor',array(Category::where('id',Vendor::where('id',$vendor->id)->get()->first()->category)->get()->first()->slug,$vendor->slug))}}">{{'<img class="img-responsive" src="data:image/jpeg;base64,' . base64_encode($vendor->avatar) . '" />'}}</a></div>
+									<div class="list-category-name">{{Vendor::find($vendor->id)->location()->get()->first()->name}}</div>
+								</div>
+								<div class="col-sm-8 col-lg-8 col-md-8">
+									<div class="caption-list">
+			                            <div class="name"><a href="{{URL::route('vendor',array(Category::where('id',Vendor::where('id',$vendor->id)->get()->first()->category)->get()->first()->slug,$vendor->slug))}}">{{$vendor->name}}</a></div>
+			                            
+			                            <?php
+			                            	
+			                            	$about=strip_tags($vendor->about);
+			                            	if($about)
+			                            	{	
+			                            		$lengthstr=strlen($about);
+			                            		if($lengthstr<210)
+			                            		{
+			                            			$shortContent=$about;
+			                            		}
+			                            		else
+			                            		{
+			                            			$pos=strpos($about, '.', 210);
+			                						$shortContent=substr($about,0,$pos )."...";
+			                						// $shortContent=$about;
+			                            		}	
+			                            		                            	
+			                            	}
+			                            	else
+			                            	{
+			                            		$shortContent="Chưa có mô tả về Dịch vụ này.";
+			                            	}	
+			                            	
+
+			                            ?>
+			                            <p>{{ $shortContent }}</p>
+			                            <div class="website">http://{{$vendor->website}}</div>
+			                        </div>
+			                        <!-- end caption-list -->
+
+									<div class="ratings">
+			                            <span class="pull-right">{{VendorComment::where('vendor',$vendor->id)->get()->count()}} Nhận xét</span>
+					   				             <p>
+					                                @if(Rating::where('vendor',$vendor->id)->get()->count()>0)
+					                                	@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1)==0)
+								  							<img src="{{Asset('images/star/0.jpg')}}" class="img-responsive agv-rating " alt="">
+								  						@else
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) > 0 & round(Rating::where('vendor',$vendor->id)->avg('rating'),1) < 1)
+									  							<img src="{{Asset('images/star/0.5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) == 1)
+									  							<img src="{{Asset('images/star/1.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) > 1 & round(Rating::where('vendor',$vendor->id)->avg('rating'),1) < 2)
+									  							<img src="{{Asset('images/star/1.5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) == 2)
+									  							<img src="{{Asset('images/star/2.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) > 2 & round(Rating::where('vendor',$vendor->id)->avg('rating'),1) < 3)
+									  							<img src="{{Asset('images/star/2.5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) == 3)
+									  							<img src="{{Asset('images/star/3.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) > 3 & round(Rating::where('vendor',$vendor->id)->avg('rating'),1) < 4)
+									  							<img src="{{Asset('images/star/3.5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) == 4)
+									  							<img src="{{Asset('images/star/4.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) > 4 & round(Rating::where('vendor',$vendor->id)->avg('rating'),1) < 5)
+									  							<img src="{{Asset('images/star/4.5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+									  						@if(round(Rating::where('vendor',$vendor->id)->avg('rating'),1) == 5)
+									  							<img src="{{Asset('images/star/5.jpg')}}" class="img-responsive agv-rating" alt="">
+									  						@endif
+								  						@endif
+					                                	@else
+					                                		<img src="{{Asset('images/star/0.jpg')}}" class="img-responsive agv-rating " alt="">
+					                                @endif
+					                            </p>
+			                        </div>
+			                        <!-- end ratings -->
+
+								    <div class="compare-list">
+								    	<label>
+									        <input type="checkbox" name="" value="" id="checkbox-list{{$vendor->id}}" class='compare-title'> Compare
+									        <input type="hidden" name="checkbox-{{$vendor->id}}" value="" >
+								        </label>
+
+								        <script type="text/javascript">
+										    $('#checkbox-list{{$vendor->id}}').on("click", function(){
+										    	$('#checkbox-photo{{$vendor->id}}').click();
+										    });
+									    </script>
+								    </div>
+								    <!-- end compare-list -->
+
+								</div>
+								<!-- end col-sm-8 col-lg-8 col-md-8 -->
+
+							</div>
+							<!-- end show-list -->
+							@endforeach
+							<!-- end results -->
+
+						@endif
+						<!-- end not in $compares -->
+
+				@endif
+				<!-- end $results != 0 -->
+			</div>
+			<!-- end .tab-list -->
+
 	</div>
 	<!-- .tab-content -->
 
