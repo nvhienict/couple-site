e0d05e4a49dd8b3f587aff2628dd79cdcd72a420
giangmd <prdonggiang@gmail.com>
2014-11-18 14:27:52 +0700

    g2

--- app/views/list-vendor.blade.php
+++ app/views/list-vendor.blade.php
@@ -311,7 +311,7 @@ Danh sách Dịch vụ
 							<!-- Vendor Images -->
 							<div class="list-avatar">
 								<a href="{{URL::route('vendor',array(VendorController::getCategorySlug( VendorController::getVendorCategory($vendor->id) ),$vendor->slug))}}">
-									{{'<img class="img-responsive" src="data:image/jpeg;base64,' . base64_encode($vendor->avatar) . '" />'}}
+									{{VendorController::getImagesVendor($vendor->photo)}}
 								</a>
 							</div>
 							
