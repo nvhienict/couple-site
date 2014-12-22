1e5d256bd784bcd138a7d9d984f6e4c89e94f2e6
tunvitc <tunvitc@gmail.com>
2014-12-22 21:50:40 +0700

    12

--- app/views/website_user/themes12/edit/photo.blade.php
+++ app/views/website_user/themes12/edit/photo.blade.php
@@ -33,32 +33,18 @@
                  
         <div class="item-title" style="text-align: {{$tabWeb->titlestyle}} font-familly: {{$website_item->font}}; color: #{{$website_item->color2}} " id = "nameTitle{{$tabWeb->id}}" > {{$tabWeb->title}}</div>  
         
-        <div class="item-content phara{{$tabWeb->id}}" >  
+        <div class="item-content phara{{$tabWeb->id}}" onclick="showckeditorpartion({{$tabWeb->id}})" data-toggle="modal" data-target='#modal-edit'>  
             <span name="phara" style="color: #{{$website_item->color3}}" >
                 {{$tabWeb->content}}
             </span>    
         </div>
-       <!--  <div class="edit-content editphara{{$tabWeb->id}}">
-            <textarea name="editor4" class="ckeditor form-control ckedit{{$tabWeb->id}}" id="editor{{$tabWeb->id}}" cols="40" rows="10" tabindex="1"></textarea>
-        </div> -->  
+        
         <div class="row phara-margin">
             <div class="col-xs-10"></div>
             <div class="col-xs-2 click-edit-hide{{$tabWeb->id}}">
-                <!-- <span><a onclick="showckeditor_text({{$tabWeb->id}})" class="glyphicon glyphicon-edit icon-site" href="javascript:void(0);"></a></span> -->
                 <span> <a style="background: #19b5bc; border:none;" onclick="showckeditorpartion({{$tabWeb->id}})" data-toggle="modal" data-target='#modal-edit' data-backdrop="static" class="btn btn-primary" href="javascript:void(0);">Sửa nội dung</a></span>
             </div>               
         </div>
-        <!-- <div class="row phara-margin">
-            <div class="col-xs-10"></div>
-            <div class="col-xs-2 ok-edit ok-edit-show{{$tabWeb->id}}">
-                <span>
-                    <a onclick="updateckeditor({{$tabWeb->id}})" class="glyphicon glyphicon-ok icon-site" href="javascript:void(0);"></a>
-                    <input type="hidden" class="get_id{{$tabWeb->id}}" value="{{$tabWeb->id}}">
-                </span>
-                <span><a style="color:#e74c3c;" onclick="exitckeditor({{$tabWeb->id}})" class=" glyphicon glyphicon-remove icon-site" href="javascript:void(0);"></a></span>
-            </div>
-        </div>  -->
-        
 
     </div>
     <div class="partion">
@@ -78,17 +64,10 @@
             <div class="col-xs-10">
             </div>
             <div class="col-xs-2">
-                <!-- <span><a  onclick="send_id_album({{$tabWeb->id}})" class="glyphicon glyphicon-edit icon-site"  data-toggle="modal" data-target='#modal-up_images' href="javascript:void(0);"></a></span> -->
                 <span><a style="background: #19b5bc; border:none;" onclick="send_id_album({{$tab->id}})" class="btn btn-primary"  data-toggle="modal" data-target='#modal-up_images' href="javascript:void(0);">Tải ảnh lên</a></span>
             </div>               
         </div>
-        <!-- <div class="row phara-margin">
-            <div class="col-xs-10"></div>
-            <div class="col-xs-2 ok-edit ">
-                <span><a  class="glyphicon glyphicon-ok icon-site" href="javascript:void(0);"></a></span>
-                <span><a class=" glyphicon glyphicon-remove icon-site" href="javascript:void(0);"></a></span>
-            </div>
-        </div>  -->
+        
     </div>
 </div>
 
