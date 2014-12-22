1c04114decdfb58828fe8104c711ef0c377b6b4e
tunvitc <tunvitc@gmail.com>
2014-12-22 22:34:48 +0700

    17

--- app/views/website_user/themes17/page/index.blade.php
+++ app/views/website_user/themes17/page/index.blade.php
@@ -32,11 +32,6 @@
         .fancybox-custom .fancybox-skin {
             box-shadow: 0 0 50px #222;
         }
-
-        .fb-comments, .fb-comments iframe[style], .fb-like-box, .fb-like-box iframe[style]
-	       {width: 100% !important;}
-	      .fb-comments span, .fb-comments iframe span[style], .fb-like-box span, .fb-like-box iframe span[style] 
-	      {width: 100% !important;}
     </style>
 
     <!-- Custom CSS -->
