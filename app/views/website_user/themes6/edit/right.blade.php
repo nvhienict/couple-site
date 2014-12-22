99e52db5fc3d757161cdd25481a1a9bc4f252516
tunvitc <tunvitc@gmail.com>
2014-12-22 17:01:58 +0700

    6

--- app/views/website_user/themes6/edit/right.blade.php
+++ app/views/website_user/themes6/edit/right.blade.php
@@ -24,28 +24,14 @@
         <div class="show-content phara{{$tab->id}}">
            <span name="phara" style="color: #{{$website_item->color3}}">{{$tab->content}}</span>
         </div>
-        <!-- <div class="edit-content editphara{{$tab->id}}">
-            <textarea name="editor{{$tab->id}}" class="ckeditor form-control ckedit{{$tab->id}}" id="editor{{$tab->id}}" cols="40" rows="10" tabindex="1">
-               {{$tab->content}}
-            </textarea>
-
-        </div> -->
+        
     </div>
     <div class="row phara-margin ">      
         <div class="click-edit click-edit-hide{{$tab->id}}" >            
-            <!-- <span> <a  onclick="showckeditor({{$tab->id}})" class="glyphicon glyphicon-edit icon-site" href="javascript:void(0);"></a></span>
-            <span><a class="glyphicon glyphicon-cog icon-site" href="javascript:void(0);"></a></span> -->
+            
             <span> <a style="background: #19b5bc; border:none;" onclick="showckeditorpartion({{$tab->id}})" data-toggle="modal" data-target='#modal-edit' data-backdrop="static" class="btn btn-primary" href="javascript:void(0);">Sửa nội dung</a></span>
         </div>
     </div>
-    <!-- <div class="row phara-margin">    
-        <div class="ok-edit ok-edit-show{{$tab->id}}">
-            <span>
-                <a onclick="updateckeditor({{$tab->id}})" class="glyphicon glyphicon-ok icon-site" href="javascript:void(0);"></a>
-                <input type="hidden" class="get_id{{$tab->id}}" value="{{$tab->id}}">
-            </span>
-            <span><a style="color:#e74c3c;" onclick="exitckeditor({{$tab->id}})" class=" glyphicon glyphicon-remove icon-site" href="javascript:void(0);"></a></span>
-        </div>
-    </div> -->
+    
     <br>
 </div>
