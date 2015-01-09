b31bf0b3d4d5b7def3a2f1bf22610306ddf8e96b
tunvitc <tunvitc@gmail.com>
2015-01-09 21:17:03 +0700

    14

--- app/views/website_user/themes14/edit/about_us.blade.php
+++ app/views/website_user/themes14/edit/about_us.blade.php
@@ -7,7 +7,7 @@
         <span onclick="sendTitle({{$tabWeb->id}},{{$tabWeb->visiable}})" class="glyphicon glyphicon-edit" data-toggle="modal" data-target='#modal-edit-menu'></span>
     </div>     
     <div class="image-couple">        
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 content-broom">        
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 content-broom text-center">        
             <span id="prev_output222">
                     @if(!empty($website_item->avatar_groom))
                     <a class="fancybox" rel="gallery1" href="{{Asset('$website_item->avatar_groom')}}" >
@@ -19,13 +19,14 @@
                      </a>   
                     @endif 
                
-                <button  onclick="send_id(null,222,0)"  data-backdrop="static" class="btn btn-primary" data-toggle="modal" data-target='#modal-changeimage' style="background: #fff;color:black; border:none;">Đổi Ảnh</button>     
+                <button  onclick="send_id(0,222)"  data-backdrop="static" class="btn btn-primary" data-toggle="modal" data-target='#modal-changeimage' style="background: #fff;color:black; border:none;">Đổi Ảnh</button>     
             </span>  
             <div class="show-content about_grooms text-center">
-                <span class="about-g">{{$website_item->about_groom}} </span>
+                <span class="about-groom">{{$website_item->about_groom}} </span>
+                <div class="text-center icon-infor"><a onclick="editInforGroom()"data-toggle="modal" data-target="#edit-infor-groom" data-backdrop="static" class="glyphicon glyphicon-edit" href="javascript:void(0);"></a></div>
             </div>
         </div>  
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center">
             <span id="prev_output111">              
                     @if(!empty($website_item->avatar_bride))
                     <a class="fancybox" rel="gallery1" href="{{Asset("$website_item->avatar_bride")}}" >
@@ -36,10 +37,11 @@
                         <img src="{{Asset("images/website/themes14/bride.jpg")}}" class="img-responsive" alt="Image">        
                      </a>   
                     @endif             
-                <button  onclick="send_id(null,111,0)"  data-backdrop="static" class="btn btn-primary" data-toggle="modal" data-target='#modal-changeimage' style="background: #fff;color:black; border:none;">Đổi Ảnh</button>
+                <button  onclick="send_id(0,111)"  data-backdrop="static" class="btn btn-primary" data-toggle="modal" data-target='#modal-changeimage' style="background: #fff;color:black; border:none;">Đổi Ảnh</button>
             </span>           
             <div class="show-content about_bride text-center">
-                <span class="about-b">{{$website_item->about_bride}}</span>
+                <span class="about-bride">{{$website_item->about_bride}}</span>
+                <div class="text-center icon-infor"><a onclick="editInforBride()" data-toggle="modal" data-target="#edit-infor-bride" data-backdrop="static" class="glyphicon glyphicon-edit" href="javascript:void(0);"></a></div>
             </div>
         </div>
         <div class="col-xs-12 col-sm-1 col-md-1 col-lg-1">
