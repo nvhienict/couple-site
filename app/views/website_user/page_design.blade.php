fe727f1d43e98c8ec2c6bf6a2b09173bbf57e28a
tunvitc <tunvitc@gmail.com>
2015-01-11 20:09:11 +0700

    update modal

--- app/views/website_user/page_design.blade.php
+++ app/views/website_user/page_design.blade.php
@@ -197,7 +197,7 @@
 			<div class="modal-content ">
 				<div class="modal-header">
 					<button type="button"  class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					<h4 class="modal-title">Chỉnh sửa tiêu đề</h4>
+					<h4 class="modal-title text-center">Chỉnh sửa tiêu đề</h4>
 				</div>
 				<div class="modal-body">
 					<div class="row">
