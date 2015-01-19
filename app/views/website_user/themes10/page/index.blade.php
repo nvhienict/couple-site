9d7b522d8b5619847acee9fb6a0dfdc7f5ff5c3c
tunvitc <tunvitc@gmail.com>
2015-01-19 08:49:24 +0700

    update 10

--- app/views/website_user/themes10/page/index.blade.php
+++ app/views/website_user/themes10/page/index.blade.php
@@ -68,20 +68,11 @@
 <div class="container">
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 index-title-temp " >
 			<!-- count datime to weddingdate -->
-				@if(empty($website_item->count_down))
 					@foreach( $date = explode('-', WebsiteController::getDates()) as $index=>$dd )
 						<div id="getD{{$index}}" style="display:none;">
 							{{$dd}}
 						</div>
 					@endforeach
-				@else
-				@foreach( $date = explode('-', WebsiteController::getCountDown()) as $index=>$dd )
-						<div id="getD{{$index}}" style="display:none;">
-							{{$dd}}
-						</div>
-					@endforeach
-				@endif
-				
 			<div id="count_dateTime">
 			
 				<table align="center" class="count_table_dateTime">
