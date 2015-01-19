07b18b40a1defaec88aca13307d67711b0947752
tunvitc <tunvitc@gmail.com>
2015-01-19 09:57:05 +0700

    update guest: check email add guest

--- app/views/guest/guest.blade.php
+++ app/views/guest/guest.blade.php
@@ -1007,6 +1007,45 @@ Danh sách khách mời
 				 $('.attend_error'+id).hide();
             	};           	
             };
+
+            $("#form_add_guest").validate({
+		rules:{
+			fullname:{
+				required:true
+			},
+			email:{
+                    
+                     email:true,
+                     remote:{
+                                url:"{{URL::route('check-email-guest')}}",
+                                type:"POST"
+                            }
+                    },
+            
+             phone:{
+             	
+                minlength:9
+
+             	},
+           
+		},
+		messages:{
+			fullname:{
+				required:"Bạn chưa nhập Tên Khách"
+			},
+			email:{
+				
+                email:"Không đúng định dạng Email",
+                remote:"Email đã tồn tại"
+			},
+			
+			phone:{
+             	
+                minlength:"Yêu cầu nhập trên 9 kí tự"
+             	},
+             
+		}
+	});
 		</script>	
 		</div>
 
--- public/assets/js/script-binh.js
+++ public/assets/js/script-binh.js
@@ -1,45 +1,6 @@
 $("#add-guest-wedding").click(function(){
 		var $validator=$("#form_add_guest").validate().resetForm();
 	});
-
-$("#form_add_guest").validate({
-		rules:{
-			fullname:{
-				required:true
-			},
-			email:{
-                    
-                     email:true,
-                     remote:{
-                                url:"{{URL::route('check-email-guest')}}",
-                                type:"POST"
-                            }
-                    },
-            
-             phone:{
-             	
-                minlength:9
-
-             	},
-           
-		},
-		messages:{
-			fullname:{
-				required:"Bạn chưa nhập Tên Khách"
-			},
-			email:{
-				
-                email:"Không đúng định dạng Email",
-                remote:"Email đã tồn tại"
-			},
-			
-			phone:{
-             	
-                minlength:"Yêu cầu nhập trên 9 kí tự"
-             	},
-             
-		}
-	});
 		function phone_validate(event)
 		      {  	
 	     if(event.which >= 37 && event.which <= 40) return;
