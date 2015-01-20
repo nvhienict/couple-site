ead299adb734a0313a1ca7ae89da848d4088b2f1
tunvitc <tunvitc@gmail.com>
2015-01-20 14:22:51 +0700

    upda 21

--- app/views/website_user/themes21/page/style-2.blade.php
+++ app/views/website_user/themes21/page/style-2.blade.php
@@ -8,7 +8,12 @@
             <span name="phara" style="color: #{{$website_item->color3}}">{{$tabWeb->content}}</span>
         </div>
         <div class="tab-text-date-time">
-            <i class="fa fa-clock-o"></i>&nbsp{{WebsiteController::getDates()}}
+            <i class="fa fa-clock-o"></i>&nbsp
+            @if(Session::has(mail))
+                {{WebsiteController::getDates()}}
+            @else
+                {{data_url}}
+            @endif
 
             <img class="tab-text-date-time-img" src="{{Asset('images/website/themes21/p3_pic4.png')}}">
         </div>
