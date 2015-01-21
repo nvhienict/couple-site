51791bdbbebdb3897d454d00ab5836e17d2467c2
tunvitc <tunvitc@gmail.com>
2015-01-21 16:44:14 +0700

    update template

--- app/views/website_user/template.blade.php
+++ app/views/website_user/template.blade.php
@@ -165,36 +165,34 @@
 	<div class="col-xs-12 lr-menu"></div><!--/.lr-menu-->
 
 	<div id='container' style="margin-top:10%;">
-		<div class="row" style="margin:0px;">
-			<div class="col-md-1"></div>
-			<div class="col-md-10">
-				<h1 style="padding-left:2%; color:#E75292; font-size:25px;">Giao diện Website</h1>
+
+		<div class="col-lg-10 col-lg-offset-1 container-content">
+			<div class="breadcrumb">
+				Giao diện website
 			</div>
-			<div class="col-md-1"></div>
-		</div>
-		<!-- .row -->
-		<div class="row" style="margin:0px;">
-			<div class="col-md-1"></div>
-			<div class="col-md-10 choose_temp">
-
-					@for($i=1; $i<=21; $i++)
-
-						<div class="col-sm-4 col-lg-4 col-md-4 hover-theme" style="margin-top:2%;">
-							<div class="span_choose_tmp">
-								<a href="javascript:void(0);">
-									<img style="width: 100%; height: 300px " src="{{Asset("images/website/tmp/{$i}.jpg")}}">
-								</a>
-							</div>
-							<a href="{{URL::route('view_theme', array('id'=>"{$i}"))}}" target="b_blank" class="btn btn-default btn-pre">Xem trước</a>		
-							<a href="{{URL::route('header-website', array('id'=>"{$i}"))}}" class="btn btn-default btn-select">Chọn</a>
-			  			</div>
-
-					@endfor
-
+			<div class="row" style="margin-top: 50px;">
+				<div class="col-xs-12">
+					<h2 style="color:#E75292; font-size:25px;">Chọn giao diện</h2>
+				</div>
 			</div>
-			<div class="col-md-1"></div>
-		</div>
-		<!-- .row -->
+
+			<div class="row" style="margin-top: 20px;">
+				
+				@for($i=1; $i<=21; $i++)
+
+					<div class="col-sm-4 col-lg-4 col-md-4 hover-theme" style="margin-top:2%;">
+						<div class="span_choose_tmp">
+							<a href="javascript:void(0);">
+								<img style="width: 100%; height: 300px " src="{{Asset("images/website/tmp/{$i}.jpg")}}">
+							</a>
+						</div>
+						<a href="{{URL::route('view_theme', array('id'=>"{$i}"))}}" target="b_blank" class="btn btn-default btn-pre">Xem trước</a>		
+						<a href="{{URL::route('header-website', array('id'=>"{$i}"))}}" class="btn btn-default btn-select">Chọn</a>
+		  			</div>
+
+				@endfor		
+			</div>
+		</div><!--/.row-->
 	</div>
 	<!-- .container -->
 
