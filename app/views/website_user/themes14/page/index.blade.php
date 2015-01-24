717b91d2001e3c7ef295f123bbe258ccbfe0c425
tunvitc <tunvitc@gmail.com>
2015-01-24 16:47:34 +0700

    14

--- app/views/website_user/themes14/page/index.blade.php
+++ app/views/website_user/themes14/page/index.blade.php
@@ -13,6 +13,7 @@
 	<meta property="og:image" content="{{Asset("{$web_fb}")}}" />
 
     <link rel="stylesheet" type="text/css" href="{{Asset("assets/css/bootstrap/bootstrap.min.css")}}">
+    <link rel="stylesheet" type="text/css" href="{{Asset("assets/css/website/template-font.css")}}">
     <script src="{{Asset("assets/js/jquery.min.js")}}"></script>
     <script type="text/javascript" src="{{Asset("assets/js/bootstrap.min.js")}}"></script>
    	
@@ -42,6 +43,20 @@
     <link rel="stylesheet" type="text/css" href="{{Asset("assets/css/style-checkbox-guestbook.css")}}">
    
 </head>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&appId=943743042306339&version=v2.0";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+</script>
+<script>
+    $(document).ready(function() {
+        $('.fb-share-button').attr("data-href", document.URL);
+    });
+</script>
 <div class="row background-themes">
 
 <!-- <img src="{{Asset("{$backgrounds}")}}" class=" img-responsive" alt="Image">	 -->
