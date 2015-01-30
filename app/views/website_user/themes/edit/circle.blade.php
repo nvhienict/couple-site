b74fac18f35955db8ddd4b315283115d53be3074
tunvitc <tunvitc@gmail.com>
2015-01-29 16:35:42 -1200

    1

--- app/views/website_user/themes/edit/circle.blade.php
+++ app/views/website_user/themes/edit/circle.blade.php
@@ -8,7 +8,9 @@
 				<img style="width: 350px;height: 350px;" class="img-responsive img-circle" src="{{Asset('images/website/themes1/boy.jpg')}}">
 				@endif
 			</a>
-			<button onclick="send_id(0,222)" data-backdrop="static" class="btn btn-primary" data-toggle="modal" data-target='#modal-changeimage' type="button" class="btn btn-primary btn-responsive">Đổi ảnh</button>		
+			<div class="text-center" style="position:relative;bottom:45px;">
+				<button onclick="send_id(0,222)" data-backdrop="static" class="btn btn-primary" data-toggle="modal" data-target='#modal-changeimage' type="button" class="btn btn-primary btn-responsive">Đổi ảnh</button>		
+			</div>
 		</div>
 	  	<div id="prev_outputcc111" class="div_circle2 col-xs-12 col-sm-3 col-lg-3 col-md-3 col-lg-offset-2 col-sm-offset-2 col-md-offset-2 text-center" >
 	  		<a href="#">
@@ -18,7 +20,9 @@
 				<img style="width: 350px;height: 350px;" class="img-responsive img-circle" src="{{Asset('images/website/themes1/girl.jpg')}}">
 				@endif
 			</a>
-			<button onclick="send_id(0,111)" data-backdrop="static" class="btn btn-primary" data-toggle="modal" data-target='#modal-changeimage' type="button" class="btn btn-primary btn-responsive">Đổi ảnh</button>
+			<div class="text-center" style="position:relative;bottom:45px;">
+				<button onclick="send_id(0,111)" data-backdrop="static" class="btn btn-primary" data-toggle="modal" data-target='#modal-changeimage' type="button" class="btn btn-primary btn-responsive">Đổi ảnh</button>
+	  		</div>
 	  	</div>
 	</div>
 </div>
